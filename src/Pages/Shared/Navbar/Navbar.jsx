923b5db83185567f25b53e87ed742d1bd046b0ac
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-05-16 08:00:53 +0600

    add a slider

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     
     </>
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-opacity-30 fixed z-10 bg-black container mx-auto text-white">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
