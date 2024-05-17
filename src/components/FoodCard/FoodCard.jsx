55478cae96b66b677bae5169a4b520c4dd821f88
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-05-17 16:36:02 +0600

    re desgin button

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -6,11 +6,11 @@ const FoodCard = ({ item }) => {
         <div className="card bg-base-100 shadow-xl">
             <figure><img className=' w-full p-4' src={image} alt="Shoes" /></figure>
             <p className='bg-slate-700 rounded-lg p-2 text-white font-medium absolute right-0 top-2'>$ {price}</p>
-            <div className="card-body text-center">
+            <div className="card-body flex flex-col items-center">
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Add To Cart</button>
+                    <button className="btn btn-outline border-0 border-b-4 mt-4 ">Add To Cart</button>
                 </div>
             </div>
         </div>
