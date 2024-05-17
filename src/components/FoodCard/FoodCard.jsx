26c9fbdb1c4a9633664af38e53db91df4df89e75
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-05-17 16:37:27 +0600

    todays last commit

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -10,7 +10,7 @@ const FoodCard = ({ item }) => {
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-outline border-0 border-b-4 mt-4 ">Add To Cart</button>
+                    <button className="btn btn-outline bg-slate-100 border-0 border-orange-400 border-b-4 mt-4 ">Add To Cart</button>
                 </div>
             </div>
         </div>
